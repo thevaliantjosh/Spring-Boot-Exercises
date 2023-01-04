6b06b67b3f9699c1167a6729d22298786bdb3969
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2023-01-04 08:30:50 -0600

    First Commit and Initializing Repo

---
+++ src/main/java/com/springboot/springbootexercises/SpringBootExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExercisesApplication.class, args);
+    }
+
+}
